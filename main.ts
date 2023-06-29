3c24ee7de5832f654666a0edd5debab327da51e1
PEConn <conn.peter@gmail.com>
2023-06-29 07:33:39 +0100

    Got the basics working.

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	Menu,
+	Notice,
+	Plugin,
+	WorkspaceLeaf,
+	TAbstractFile
+} from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
@@ -10,73 +16,138 @@ const DEFAULT_SETTINGS: MyPluginSettings = {
 	mySetting: 'default'
 }
 
+interface FileData {
+	created: number
+}
+
+type FileDataMap = {
+	[path: string]: FileData
+}
+
+interface FileItem {
+	titleEl?: HTMLElement;
+	selfEl: HTMLElement;
+}
+
+const archiveRoot = "4 - archive";
+
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		this.app.workspace.on("file-menu", (menu: Menu, file: TAbstractFile, source: string, leaf: WorkspaceLeaf | undefined) => {
+			if (file.path === archiveRoot) return;
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+			const inArchive = file.path.startsWith(archiveRoot + "/");
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+			menu.addItem((item) => {
+				const title = inArchive ? "Unarchive" : "Archive";
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+				item.setTitle(title)
+					.onClick(() => {
+						const dest = inArchive ? file.path.substring(archiveRoot.length + 1) : archiveRoot + "/" + file.path;
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+						this.moveToDest(file, dest);
+					})
+			})
+		})
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.setUiAttributes();
 	}
+
+	private async moveToDest(file: TAbstractFile, dest: string) {
+		// It seems on windows (where the path separater is '\') the files still use '/' as the separater.
+		const pathSep = "/";
+		const parts = dest.split(pathSep);
+
+		let pathSoFar = "";
+		for (let i = 0; i < parts.length - 1; i++) {
+			try {
+				pathSoFar += parts[i];
+				console.log("Creating " + pathSoFar);
+				await this.app.vault.createFolder(pathSoFar);
+			} catch (err) {
+				// The folder already exists.
+			} finally {
+				pathSoFar += pathSep;
+			}
+		}
+
+		try {
+			await this.app.vault.rename(file, dest);
+		} catch (err) {
+			new Notice("Failed.");
+			console.log(err);
+		}
+	}
+
+	public async setUiAttributes() {
+		try {
+			await this.setUiAttributesInner();
+		} catch (err) {
+			console.log(err);
+			console.log("Trying again");
+			setTimeout(() => {
+				this.setUiAttributes();
+			}, 500)
+		}
+	}
+
+	public async setUiAttributesInner() {
+		const fileExplorerLeaf = await this.getFileExplorerLeaf();
+
+		const fileDataMap: FileDataMap = {};
+
+		this.app.vault.getFiles().forEach((file) => {
+			fileDataMap[file.path] = { created: file.stat.ctime }
+		});
+
+		console.log(fileDataMap);
+
+		const fileItems: { [path: string]: FileItem } = (
+			fileExplorerLeaf.view as any
+		).fileItems;
+
+		for (const path in fileItems) {
+			const item = fileItems[path];
+
+			if (fileDataMap[path]) {
+				(item.titleEl ?? item.selfEl).setAttribute(
+					"peters-tool-date",
+					new Date(fileDataMap[path].created).toLocaleDateString(),
+				);
+			} else {
+				console.log("Could not find " + path);
+			}
+		}
+	}
+
+	private async getFileExplorerLeaf(): Promise<WorkspaceLeaf> {
+		return new Promise((resolve, reject) => {
+			let foundLeaf: WorkspaceLeaf | null = null;
+			this.app.workspace.iterateAllLeaves((leaf) => {
+				if (foundLeaf) {
+					return;
+				}
+
+				const view = leaf.view as any;
+				if (!view || !view.fileItems) {
+					return;
+				}
+
+				foundLeaf = leaf;
+				resolve(foundLeaf);
+				console.log("Found File Explorer Leaf");
+			});
+
+			if (!foundLeaf) {
+				reject(Error("Could not find file explorer leaf."));
+			}
+		});
+	}
+
 
 	onunload() {
 
@@ -88,50 +159,5 @@ export default class MyPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
