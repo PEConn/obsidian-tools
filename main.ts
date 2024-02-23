b4a7c442355bcbb5dcf722d14d362e946e67bfc6
Peter E Conn <conn.peter@gmail.com>
2024-02-23 16:54:43 +0000

    Reformat main.ts

--- main.ts
+++ main.ts
@@ -1,61 +1,61 @@
 import {
   Editor,
-	Menu,
-	Notice,
-	Plugin,
-	WorkspaceLeaf,
-	TAbstractFile
+  Menu,
+  Notice,
+  Plugin,
+  WorkspaceLeaf,
+  TAbstractFile
 } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-	mySetting: string;
+  mySetting: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+  mySetting: 'default'
 }
 
 interface FileData {
-	created: number
+  created: number
 }
 
 type FileDataMap = {
-	[path: string]: FileData
+  [path: string]: FileData
 }
 
 interface FileItem {
-	titleEl?: HTMLElement;
-	selfEl: HTMLElement;
+  titleEl?: HTMLElement;
+  selfEl: HTMLElement;
 }
 
 const archiveRoot = "4 - archive";
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+  settings: MyPluginSettings;
 
-	async onload() {
-		await this.loadSettings();
+  async onload() {
+    await this.loadSettings();
 
-		this.app.workspace.on("file-menu", (menu: Menu, file: TAbstractFile, source: string, leaf: WorkspaceLeaf | undefined) => {
-			if (file.path === archiveRoot) return;
+    this.app.workspace.on("file-menu", (menu: Menu, file: TAbstractFile, source: string, leaf: WorkspaceLeaf | undefined) => {
+      if (file.path === archiveRoot) return;
 
-			const inArchive = file.path.startsWith(archiveRoot + "/");
+      const inArchive = file.path.startsWith(archiveRoot + "/");
 
-			menu.addItem((item) => {
-				const title = inArchive ? "Unarchive" : "Archive";
+      menu.addItem((item) => {
+        const title = inArchive ? "Unarchive" : "Archive";
         const icon = inArchive ? "archive-restore" : "archive";
 
-				item.setTitle(title)
-          .setIcon(icon)
-					.onClick(() => {
-						const dest = inArchive ? file.path.substring(archiveRoot.length + 1) : archiveRoot + "/" + file.path;
+        item.setTitle(title)
+        .setIcon(icon)
+        .onClick(() => {
+          const dest = inArchive ? file.path.substring(archiveRoot.length + 1) : archiveRoot + "/" + file.path;
 
-						this.moveToDest(file, dest);
-					})
-			})
-		})
+          this.moveToDest(file, dest);
+        })
+      })
+    })
 
     this.addCommand({
       id: "sort-tasks",
@@ -63,8 +63,8 @@ export default class MyPlugin extends Plugin {
       editorCallback: (editor: Editor) => { this.sortTodos(editor); }
     })
 
-		this.setUiAttributes();
-	}
+    this.setUiAttributes();
+  }
 
   private sortTodos(editor: Editor) {
     console.log("Sorting...");
@@ -131,107 +131,107 @@ export default class MyPlugin extends Plugin {
     return line.trimStart().startsWith("- [");
   }
 
-	private async moveToDest(file: TAbstractFile, dest: string) {
-		// It seems on windows (where the path separater is '\') the files still use '/' as the separater.
-		const pathSep = "/";
-		const parts = dest.split(pathSep);
+  private async moveToDest(file: TAbstractFile, dest: string) {
+    // It seems on windows (where the path separater is '\') the files still use '/' as the separater.
+    const pathSep = "/";
+    const parts = dest.split(pathSep);
 
-		let pathSoFar = "";
-		for (let i = 0; i < parts.length - 1; i++) {
-			try {
-				pathSoFar += parts[i];
-				console.log("Creating " + pathSoFar);
-				await this.app.vault.createFolder(pathSoFar);
-			} catch (err) {
-				// The folder already exists.
-			} finally {
-				pathSoFar += pathSep;
-			}
-		}
+    let pathSoFar = "";
+    for (let i = 0; i < parts.length - 1; i++) {
+      try {
+        pathSoFar += parts[i];
+        console.log("Creating " + pathSoFar);
+        await this.app.vault.createFolder(pathSoFar);
+      } catch (err) {
+        // The folder already exists.
+      } finally {
+        pathSoFar += pathSep;
+      }
+    }
 
-		try {
-			await this.app.vault.rename(file, dest);
-		} catch (err) {
-			new Notice("Failed.");
-			console.log(err);
-		}
-	}
+    try {
+      await this.app.vault.rename(file, dest);
+    } catch (err) {
+      new Notice("Failed.");
+      console.log(err);
+    }
+  }
 
-	public async setUiAttributes() {
-		try {
-			await this.setUiAttributesInner();
-		} catch (err) {
-			console.log(err);
-			console.log("Trying again");
-			setTimeout(() => {
-				this.setUiAttributes();
-			}, 500)
-		}
-	}
+  public async setUiAttributes() {
+    try {
+      await this.setUiAttributesInner();
+    } catch (err) {
+      console.log(err);
+      console.log("Trying again");
+      setTimeout(() => {
+        this.setUiAttributes();
+      }, 500)
+    }
+  }
 
-	public async setUiAttributesInner() {
-		const fileExplorerLeaf = await this.getFileExplorerLeaf();
+  public async setUiAttributesInner() {
+    const fileExplorerLeaf = await this.getFileExplorerLeaf();
 
-		const fileDataMap: FileDataMap = {};
+    const fileDataMap: FileDataMap = {};
 
-		this.app.vault.getFiles().forEach((file) => {
-			fileDataMap[file.path] = { created: file.stat.ctime }
-		});
+    this.app.vault.getFiles().forEach((file) => {
+      fileDataMap[file.path] = { created: file.stat.ctime }
+    });
 
-		console.log(fileDataMap);
+    console.log(fileDataMap);
 
-		const fileItems: { [path: string]: FileItem } = (
-			fileExplorerLeaf.view as any
-		).fileItems;
+    const fileItems: { [path: string]: FileItem } = (
+      fileExplorerLeaf.view as any
+    ).fileItems;
 
-		for (const path in fileItems) {
-			const item = fileItems[path];
+    for (const path in fileItems) {
+      const item = fileItems[path];
 
-			if (fileDataMap[path]) {
-				(item.titleEl ?? item.selfEl).setAttribute(
-					"peters-tool-date",
-					new Date(fileDataMap[path].created).toLocaleDateString(),
-				);
-			} else {
-				console.log("Could not find " + path);
-			}
-		}
-	}
+      if (fileDataMap[path]) {
+        (item.titleEl ?? item.selfEl).setAttribute(
+          "peters-tool-date",
+          new Date(fileDataMap[path].created).toLocaleDateString(),
+        );
+      } else {
+        console.log("Could not find " + path);
+      }
+    }
+  }
 
-	private async getFileExplorerLeaf(): Promise<WorkspaceLeaf> {
-		return new Promise((resolve, reject) => {
-			let foundLeaf: WorkspaceLeaf | null = null;
-			this.app.workspace.iterateAllLeaves((leaf) => {
-				if (foundLeaf) {
-					return;
-				}
+  private async getFileExplorerLeaf(): Promise<WorkspaceLeaf> {
+    return new Promise((resolve, reject) => {
+      let foundLeaf: WorkspaceLeaf | null = null;
+      this.app.workspace.iterateAllLeaves((leaf) => {
+        if (foundLeaf) {
+          return;
+        }
 
-				const view = leaf.view as any;
-				if (!view || !view.fileItems) {
-					return;
-				}
+        const view = leaf.view as any;
+        if (!view || !view.fileItems) {
+          return;
+        }
 
-				foundLeaf = leaf;
-				resolve(foundLeaf);
-				console.log("Found File Explorer Leaf");
-			});
+        foundLeaf = leaf;
+        resolve(foundLeaf);
+        console.log("Found File Explorer Leaf");
+      });
 
-			if (!foundLeaf) {
-				reject(Error("Could not find file explorer leaf."));
-			}
-		});
-	}
+      if (!foundLeaf) {
+        reject(Error("Could not find file explorer leaf."));
+      }
+    });
+  }
 
 
-	onunload() {
+  onunload() {
 
-	}
+  }
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 }
